9e59d0247cfd2a11aff51820bc9923773eb2c369
gcairesdev <pessoalgcaires@gmail.com>
2019-11-04 09:21:42 -0200

    Arquivo de conexao criado

--- Model/Connect.php
+++ Model/Connect.php
@@ -2,11 +2,19 @@
 
     class Connect {
         
+        $host    = "localhost";        
+        $usuario = "root";        
+        $senha   = "";        
+        $banco   = "liomagazine";        
+        $mysqli;
+
         public function connect() {
-            
+            $this->mysqli = new mysqli($this->host, $this->usuario, $this->senha, $this->banco);
         }
 
+        public function disconnect() {
+            $this->mysqli->close();
+        }
     }
     
-
 ?>
