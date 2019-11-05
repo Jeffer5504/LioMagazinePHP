8f8a03694c7bb35ae6119920f0af5ee6aadae498
Laércio Marques <laerciom99@gmail.com>
2019-11-04 23:30:45 -0300

    Cadastro Venda

---
+++ Model/Venda.php
@@ -0,0 +1,10 @@
+<?php
+
+class Venda extends Connect{
+
+    public function insert($query){
+        $this->connect();
+        $this->query = $this->mysqli->query($query);
+        $this->disconnect();
+    }
+}
